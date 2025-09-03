7bb8b0e58270d06418a4bf5b67ec534efebefe05
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-09-04 04:46:13 +0600

    feat(WelcomePage): add continue button

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -1,12 +1,27 @@
-
-
 const WelcomePage = () => {
-    return (
-        <div className="text-xl text-red-500">
-            this is a welcome page 
-            
+  return (
+    <div
+      className="text-xl text-red-500 h-screen flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/welcome.png')" }}
+    >
+      <div className="text-center">
+        <p className="lg:text-7xl text-3xl md:text-5xl lg:w-[1025px] font-bold">
+          Welcome to Extra Handen’s <br />
+          <span>AI tool</span>
+        </p>
+        {/* Button */}
+        <div className="flex justify-between gap-50">
+          <button className="btn lg:w-[265px] lg:h-[68px] rounded-full  mt-6 mx-auto flex justify-between items-center px-4 py-2">
+            <span className="text-xl">Continue</span>
+            <img
+              src="/images/rightIcons.png"
+              alt="Right Icon"
+              className="w-12 h-12 bg-[#13072E] rounded-full"
+            />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-
 export default WelcomePage;
