22e1185c98f56aab5f6d391d6f2e9d8434a08c9d
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-09-04 05:55:30 +0600

    feat(create): Navbar and login page create

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,11 +1,20 @@
 
-const Login = () => {
-    return (
-        <div>
-            This is a login page 
-            
-        </div>
-    );
-};
 
+const Login = () => {
+  return (
+    <div
+      className="text-xl text-white h-screen flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/background.png')" }}
+    >
+      <div className="text-center">
+        <p className="lg:text-7xl text-3xl md:text-5xl lg:w-[1025px] font-bold">
+          Welcome to Extra Handen’s <br />
+          <span>AI tool</span>
+        </p>
+        {/* Button */}
+   
+      </div>
+    </div>
+  );
+};
 export default Login;
