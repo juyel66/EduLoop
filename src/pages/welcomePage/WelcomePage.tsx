ec9c3ba30cf51f5fa79850faf9f35277b9c038bd
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-09-04 05:57:20 +0600

    fix(WelcomePage and routes): banner and route changes

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router";
+
 const WelcomePage = () => {
   return (
     <div
-      className="text-xl text-red-500 h-screen flex items-center justify-center bg-cover bg-center"
+      className="text-xl text-white h-screen flex items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: "url('/images/welcome.png')" }}
     >
       <div className="text-center">
@@ -11,14 +13,14 @@ const WelcomePage = () => {
         </p>
         {/* Button */}
         <div className="flex justify-between gap-50">
-          <button className="btn lg:w-[265px] lg:h-[68px] rounded-full  mt-6 mx-auto flex justify-between items-center px-4 py-2">
+          <Link to='/login'  className="btn lg:w-[265px] lg:h-[68px] rounded-full  mt-6 mx-auto flex justify-between items-center px-4 py-2">
             <span className="text-xl">Continue</span>
             <img
               src="/images/rightIcons.png"
               alt="Right Icon"
               className="w-12 h-12 bg-[#13072E] rounded-full"
             />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,22 @@
-
-
 import { createBrowserRouter } from "react-router";
+import Login from "../pages/Auth/Login";
+import RootLayout from "../Components/layout/RootLayout";
 import WelcomePage from "../pages/WelcomePage/welcomePage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <WelcomePage></WelcomePage>
-  }
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/", 
+        element: <WelcomePage />,
+      },
+      {
+        path: "/login", 
+        element: <Login />,
+      },
+      
+    ],
+  },
 ]);
