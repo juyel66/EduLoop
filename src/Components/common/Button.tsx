cf38bd31dad8ee7baa832a750bf0ade1ca8b6117
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-09-05 00:17:21 +0600

    feat(Button) Common button add

---
+++ src/Components/common/Button.tsx
@@ -0,0 +1,16 @@
+const Button = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-6 bg-[#E16641]  p-2  w-36 rounded-2xl">
+        <img
+          className="bg-white p-2 rounded-xl"
+          src="/public/images/leftIcons.png"
+          alt=""
+        />
+        <p>Back</p>
+      </div>
+    </div>
+  );
+};
+
+export default Button;
